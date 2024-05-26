1c052afca3dbf6becefdf099844554be8a0ea5a4
EvelymVasconcelos <evelym.vasconcelos@gmail.com>
2024-05-26 00:07:35 -0400

    Add main class

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DesafioPraticoCSharp;
+
+class Program
+{
+    static void Main() {
+        Console.WriteLine("Desafio1:");
+        Desafio1.ImprimirNomeUsuario();
+
+        Console.WriteLine("\nDesafio2:");
+        Desafio2.ImprimirNomeSobrenomeUsuario();
+
+        Console.WriteLine("\nDesafio3:");
+        Desafio3.RealizarOperacoesMatematicas();
+
+        Console.WriteLine("\nDesafio4:");
+        Desafio4.ContarCaracteres();
+
+        Console.WriteLine("\nDesafio5:");
+        Desafio5.InserirPlaca();
+
+        Console.WriteLine("\nDesafio6:");
+        Desafio6.ExibirDataAtual();
+    }
+}
