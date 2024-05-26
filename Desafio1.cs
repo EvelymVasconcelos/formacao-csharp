8bcae319b1716ebf7867d9f5914d4c6b07768ca7
EvelymVasconcelos <evelym.vasconcelos@gmail.com>
2024-05-26 00:03:58 -0400

    Add desafio1

---
+++ Desafio1.cs
@@ -0,0 +1,10 @@
+﻿namespace DesafioPraticoCSharp;
+public class Desafio1
+{
+    public static void ImprimirNomeUsuario() {
+        Console.WriteLine("Digite o seu nome:");
+        string nome = Console.ReadLine()!;
+        string resultado = nome != "" ? $"Olá, {nome.ToUpper()}! Seja muito bem-vindo!" : "Nome nao informado";
+        Console.WriteLine(resultado);
+    }   
+}
